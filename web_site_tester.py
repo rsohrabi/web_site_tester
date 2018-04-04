a3c4cdfe4878956fed40302f940888f491458116
rocky sohrabi <rockysohrabi@hotmail.com>
2018-04-04 08:19:01 -0500

    Basic web tester to work with request package.

--- web_site_tester.py
+++ web_site_tester.py
@@ -13,33 +13,39 @@ HORIZONTAL_DASH = 60
 ALLOW_REDIRECTS = False
 TIME_PLACES = 3
 
-# input must have at least 2 inputs.
-if len(sys.argv) < 2:
-    print("Usage: {} site1 site2 site2...".format(sys.argv[0]))
-    print("Verify input web sites are online.")
-    sys.exit(0)
 
-print("Checking web site status for:")
-print("   ".join(sys.argv[1:]))
+def check_web():
+    # input must have at least 2 inputs.
+    if len(sys.argv) < 2:
+        print("Usage: {} site1 site2 site2...".format(sys.argv[0]))
+        print("Verify input web sites are online.")
+        sys.exit(0)
 
-print("-" * HORIZONTAL_DASH)
-try:
-    for site in sys.argv[1:]:
-        tc1 = time.clock()
-        r = requests.head(site, allow_redirects=ALLOW_REDIRECTS)
-        tc2 = time.clock()
-        print("Site URL:", site)
-        print("Returned results:")
-        print("return.ok:", r.ok)
-        print("return.status_code:", r.status_code)
-        print("Web site response time:", round(tc2 - tc1, TIME_PLACES), "secs")
-        print("-" * HORIZONTAL_DASH)
-except requests.ConnectionError as ce:
-    print("Error: ConnectionError: {}".format(ce))
-    sys.exit(1)
-except requests.exceptions.MissingSchema as ms:
-    print("Error: MissingSchema: {}".format(ms))
-    sys.exit(1)
-except Exception as e:
-    print("Error: Exception: {}".format(e))
-    sys.exit(1)
+    print("Checking web site status for:")
+    print("   ".join(sys.argv[1:]))
+
+    print("-" * HORIZONTAL_DASH)
+    try:
+        for site in sys.argv[1:]:
+            tc1 = time.clock()
+            r = requests.head(site, allow_redirects=ALLOW_REDIRECTS)
+            tc2 = time.clock()
+            print("Site URL:", site)
+            print("Returned results:")
+            print("return.ok:", r.ok)
+            print("return.status_code:", r.status_code)
+            print("Web site response time:", round(tc2 - tc1, TIME_PLACES), "secs")
+            print("-" * HORIZONTAL_DASH)
+    except requests.ConnectionError as ce:
+        print("Error: ConnectionError: {}".format(ce))
+        sys.exit(1)
+    except requests.exceptions.MissingSchema as ms:
+        print("Error: MissingSchema: {}".format(ms))
+        sys.exit(1)
+    except Exception as e:
+        print("Error: Exception: {}".format(e))
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    check_web()
